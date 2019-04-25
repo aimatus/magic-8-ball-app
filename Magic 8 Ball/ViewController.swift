eed27074f35c17d5826885bd703450a2cfab49e2
Abraham Matus <aimatusco@gmail.com>
2019-04-25 15:34:36 -0500

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Abraham Matus on 2019-04-25.
+//  Copyright © 2019 Abraham Matus. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
