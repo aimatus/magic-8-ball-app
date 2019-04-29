336e5a19559ed5f23a5fd95edffd76034ca18781
Abraham Matus <aimatusco@gmail.com>
2019-04-29 17:21:16 -0500

    Adds 8 Ball functionality

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,11 +11,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let DICE_IMAGE_NAME_PREFIX: String = "dice"
+    let START_RANDOM_NUMBER: Int = 1
+    let LAST_RANDOM_NUMBER: Int = 5
+
+    @IBOutlet weak var eightBallImageView: UIImageView!
+
     override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        generateNew8BallResponse()
     }
 
+    private func generateNew8BallResponse() {
+        eightBallImageView.image = getRandomImage()
+    }
 
+    @IBAction func onAskButtonPressed(_ sender: Any) {
+        generateNew8BallResponse()
+    }
 
+    private func getRandomImage() -> UIImage? {
+        let imageNumber: Int = Int.random(in: START_RANDOM_NUMBER...LAST_RANDOM_NUMBER)
+        let eightBallImage: String = "ball\(imageNumber)"
+        return UIImage(named: eightBallImage);
+    }
+
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        generateNew8BallResponse()
+    }
 }
